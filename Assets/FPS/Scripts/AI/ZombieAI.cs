983219e93a6785e477b220e584a2af4f8cb4b56b
luke dalgleish <lukedalgleish07@gmail.com>
2025-08-11 22:22:08 +0100

    Zombie initial commit
    Zombie prefab, animations. I hooked up the code for the idle and patrolling state too.

--- Assets/FPS/Scripts/AI/ZombieAI.cs
+++ Assets/FPS/Scripts/AI/ZombieAI.cs
@@ -4,30 +4,105 @@ using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
-    public Transform player;
+    public ZombieState currentState = ZombieState.Idle;
     public float roamRadius = 10f;
     public float roamInterval = 5f;
-
+    public float timeSpentIdle = 10f;
+    
     private NavMeshAgent agent;
+    private Animator animator;
+
     private float roamTimer;
+    private float idleTimer = 0;
+    private bool hasPatrollingDestination;
+
+    public enum ZombieState
+    {
+        Idle,
+        Patrolling,
+        Chasing,
+        Attacking,
+        Dead
+    }
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-
+        animator = GetComponent<Animator>();
         roamTimer = roamInterval;
     }
 
-    void Update()
+    private void Update()
     {
-        roamTimer += Time.deltaTime;
+        switch (currentState)
+        {
+            case ZombieState.Idle:
+                HandleIdle();
+                break;
+            case ZombieState.Patrolling:
+                HandlePatrolling();
+                break;
+            case ZombieState.Chasing:
+                HandleChasing();
+                break;
+            case ZombieState.Attacking:
+                HandleAttacking();
+                break;
+            case ZombieState.Dead:
+                HandleDead();
+                break;
+        }
+    }
 
-        if (roamTimer >= roamInterval && agent.remainingDistance < 0.5f)
+    private void HandleDead()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void HandleAttacking()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void HandleChasing()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void HandlePatrolling()
+    {
+        Debug.Log("Zombie is patrolling.");
+        animator.SetBool("Running", true);
+        if (hasPatrollingDestination == false)
         {
             Vector3 newPos = RandomNavSphere(transform.position, roamRadius, -1);
             agent.SetDestination(newPos);
-            roamTimer = 0;
+            hasPatrollingDestination = true;
         }
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            currentState = ZombieState.Idle;
+            hasPatrollingDestination = false;
+        }
+
+    }
+
+    private void HandleIdle()
+    {
+        Debug.Log("Zombie is idle.");
+
+        idleTimer += Time.deltaTime;
+        animator.SetBool("Running", false);
+        animator.SetBool("Idle", true);
+
+        if (idleTimer >= timeSpentIdle)
+        {
+            animator.SetBool("Idle", false);
+            currentState = ZombieState.Patrolling;
+            idleTimer = 0;
+        }
+
     }
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
